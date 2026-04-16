39316c298abd945276c233d41fb292b7e7b0b9e0
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-16 13:59:47 +0300

    ajout de express

--- app.js
+++ app.js
@@ -4,3 +4,17 @@
 
 // on importe le module express qui nous permettra de créer une application web
 const express = require('express');
+
+// on crée une application express en appelant la fonction express()
+const app = express();
+
+
+
+
+
+
+
+
+
+// on exporte notre application express pour pouvoir l'utiliser dans le fichier myserver.js
+module.exports = app;
