7ce77d69defce08ff06ed0a9c623f683433058c3
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-16 11:10:15 +0300

    serveur pour crée mon serveur

---
+++ app.js
@@ -0,0 +1,6 @@
+/**
+ * Le fichier app.js est une application Express qui gère les routes de notre serveur web.
+ */
+
+// on importe le module express qui nous permettra de créer une application web
+const express = require('express');
---
+++ myserver.js
@@ -0,0 +1,18 @@
+/**
+ * le fichier myserver.js a pour mission de créer un serveur 
+ */
+
+// on importe le module http de node.js qui nous permettra de créer un serveur web
+const http = require('http');
+
+// on importe notre application express qui gère les routes de notre serveur web
+const app = require('./app');
+
+// on crée un serveur en utilisant la méthode createServer du module http
+const server = http.createServer(app);
+
+const numPort = 3009;
+
+server.listen("numPort", () => {
+    console.log("Serveur démarré sur le port " , numPort);
+});
