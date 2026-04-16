abb6eeee0e3e8060af22c51165ad277cb5d4a60c
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-16 16:14:50 +0300

    création de la route pour l'authentification

---
+++ routes/authentificationRoute.js
@@ -0,0 +1,15 @@
+/**
+ * Le fichier autentificationRoute.js est un module qui définit les routes pour l'authentification des utilisateurs
+ */
+
+// on importe le module express qui nous permettra de créer une application web
+const express = require('express');
+
+// on crée un routeur express en appelant la fonction express.Router()
+const router = express.Router();
+const authentificationController = require('../controllers/authentificationController');    
+
+// route pour la page d'inscription ex:localhost:3009/register
+router.get('/registre', authentificationController.registerView); // route pour la page d'inscription
+
+module.exports = router;
