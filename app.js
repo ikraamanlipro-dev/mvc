248273118f2ee3b393e3f0ba82708ffaf781d1ad
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-16 16:10:21 +0300

    ajout route authentification

--- app.js
+++ app.js
@@ -5,8 +5,7 @@
 // on importe le module express qui nous permettra de créer une application web
 const express = require('express');
 
-// on importe la route pour la route d'accueil
-const accueilRoute = require('./routes/accueilRoute');
+
 
 // on crée une application express en appelant la fonction express()
 const app = express();
@@ -15,8 +14,19 @@ app.set('views', './views'); // on indique à express où se trouvent les vues
 app.set('view engine', 'ejs'); // on indique à express que nous utilisons le moteur de template ejs
 
 
+// ==== accueilRoute ====
+
+// on importe la route pour la route d'accueil
+const accueilRoute = require('./routes/accueilRoute');
+
 // on utilise la route d'accueil pour la racine de notre site web
 app.use('/', accueilRoute); // on utilise la route d'accueil pour la racine de notre site web
+
+// ==== authentificationRoute ====
+const authentificationRoute = require('./routes/authentificationRoute'); // on importe la route pour l'authentification
+
+// on utilise la route pour l'authentification pour la racine de notre site web
+app.use('/', authentificationRoute); // on utilise la route pour l'authentification pour la racine de notre site web
 
 // on exporte notre application express pour pouvoir l'utiliser dans le fichier myserver.js
 module.exports = app;
