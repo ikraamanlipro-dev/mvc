ec22fa5cd7781906a94a4e66383a17a3aa0d6f07
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-16 16:09:44 +0300

    ajout

--- app.js
+++ app.js
@@ -5,16 +5,18 @@
 // on importe le module express qui nous permettra de créer une application web
 const express = require('express');
 
+// on importe la route pour la route d'accueil
+const accueilRoute = require('./routes/accueilRoute');
+
 // on crée une application express en appelant la fonction express()
 const app = express();
 
+app.set('views', './views'); // on indique à express où se trouvent les vues
+app.set('view engine', 'ejs'); // on indique à express que nous utilisons le moteur de template ejs
 
 
-
-
-
-
-
+// on utilise la route d'accueil pour la racine de notre site web
+app.use('/', accueilRoute); // on utilise la route d'accueil pour la racine de notre site web
 
 // on exporte notre application express pour pouvoir l'utiliser dans le fichier myserver.js
 module.exports = app;
