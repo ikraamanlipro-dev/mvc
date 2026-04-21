f8312aa648e430e06220f840f4d157e7f87fc3c9
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-21 07:21:44 +0300

    Création dossier

--- routes/authentificationRoute.js
+++ routes/authentificationRoute.js
@@ -7,9 +7,13 @@ const express = require('express');
 
 // on crée un routeur express en appelant la fonction express.Router()
 const router = express.Router();
+
 const authentificationController = require('../controllers/authentificationController');    
 
 // route pour la page d'inscription ex:localhost:3009/register
 router.get('/registre', authentificationController.registerView); // route pour la page d'inscription
+
+// route pour enregistrer un nouvel utilisateur dans la base de données
+router.post('/registre', authentificationController.registerUser); // route pour enregistrer un nouvel utilisateur dans la base de données
 
 module.exports = router;
