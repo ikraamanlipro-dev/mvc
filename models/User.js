1a0ae620a2d02097a72e029af8e4fb378d53023c
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-21 16:28:36 +0300

    Configuration avec la bd

--- models/User.js
+++ models/User.js
@@ -9,20 +9,36 @@ const Datatypes = require('sequelize');
 // on importe la connexion à la base de données
 const sequelize = require('../db');
 
-module.exports = sequelize.define(
-    'user', // nom de la table dans la base de données
-    {
-        id: {
-            type: Datatypes.INTEGER, // type de données pour l'id
-            primaryKey: true, // clé primaire
-            autoIncrement: true // incrémentation automatique
-        },
+// module.exports = sequelize.define(
+//     'user', // nom de la table dans la base de données
+//     {
+//         id: {
+//             type: Datatypes.INTEGER, // type de données pour l'id
+//             primaryKey: true, // clé primaire
+//             autoIncrement: true // incrémentation automatique
+//         },
+//         email: {
+//             type: Datatypes.STRING, // type de données pour l'email
+//             unique: true // l'email doit être unique
+//         },
+//         password: {
+//             type: Datatypes.STRING // type de données pour le mot de passe
+//         }
+//     }
+// );
+
+module.exports = (sequelize,Sequelize) => {
+    const UserModel = sequelize.define('user', {
         email: {
-            type: Datatypes.STRING, // type de données pour l'email
+            type: Sequelize.STRING, // type de données pour l'email
             unique: true // l'email doit être unique
         },
         password: {
-            type: Datatypes.STRING // type de données pour le mot de passe
+            type: Sequelize.STRING // type de données pour le mot de passe
         }
-    }
-);
+    });
+
+    return UserModel;
+}
+
+
