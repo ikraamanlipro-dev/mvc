66d93e09aa9eab9ba76e40fff9b33e531d33b2a1
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-24 06:46:11 +0300

    Réagencement du code

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.set('view engine', 'ejs'); // on indique à express que nous utilisons le mo
 
 app.use(express.static('public')); // on indique à express où se trouvent les fichiers statiques (css, js, images)
 
-app.use(express.urlencoded({ extended: false })); // extrait les donnée saisies dans les formulaires 
+app.use(express.json()); // extrait les données au format JSON
+app.use(express.urlencoded({ extended: false })); // extrait les donnée saisies dans les formulaires
 
 app.use(expressMyConnection(mySql2, dbConfig, 'pool'));
 
